e5e3d305e6b273c1bafb01d56d673b6b9a841b1d
Levon <levon16.01.99@gmail.com>
2022-02-10 18:48:43 +0400

    first commit

---
+++ js/sctipt.js
@@ -0,0 +1,21 @@
+//"use strict";
+
+console.log('arr' + " - object");
+console.log(4 +  "5");
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr,--decr);
+
+console.log(5%3);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+        isClose = false;
+
+console.log(isChecked || !isClose);
